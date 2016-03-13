e6de5234b6cf4adc7320f7a0c2035924c7928a7c
EslamYoussef <eslam.youssef.m@gmail.com>
2016-03-14 00:58:05 +0200

    Adding "My Portfolio APP"

---
+++ Projects/My_Prtfolio_APP/app/src/main/java/com/andnd/me/myappportfolio/MainActivity.java
@@ -0,0 +1,20 @@
+package com.andnd.me.myappportfolio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v) {
+        Toast.makeText(this, "The button will lunch application : " + ((Button) (v)).getText(), Toast.LENGTH_LONG).show();
+    }
+}
