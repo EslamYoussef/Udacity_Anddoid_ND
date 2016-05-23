5c54cfcb5a5a2c594b00ec36449f4d47ca1db6e8
Eslam <eslam.youssef.m@gmail.com>
2016-05-23 02:49:42 +0200

    Edit permissions

---
+++ Projects/MovieApp/app/src/main/java/com/udacity/eslam/Views/MainActivity.java
@@ -0,0 +1,21 @@
+package com.udacity.eslam.Views;
+
+import android.app.DownloadManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.udacity.eslam.Models.Movie;
+import com.udacity.eslam.movieapp.R;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
